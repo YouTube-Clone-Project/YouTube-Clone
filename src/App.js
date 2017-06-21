58ffada75d541412d163b481f02a7d3d56188b32
Jessica <agro-chick@hotmail.com>
2017-06-21 09:42:37 -0600

    'changed App.js for header and footer'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import './reset.css';
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-
+        <Header />
         <div className="App-header">
           <h2>YouTube Clone</h2>
         </div>
-
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+export default function header(props) {
+    super(props);
+    
+}
