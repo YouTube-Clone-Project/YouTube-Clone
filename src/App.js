5a28cbb622b76ef830fd71da210b1223410cd778
Jessica <agro-chick@hotmail.com>
2017-06-21 16:21:22 -0600

    'fixed app.js'

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-          {/*{ router }*/}
+          { router }
         <Footer />
       </div>
     );
