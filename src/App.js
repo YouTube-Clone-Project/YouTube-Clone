37e052755c30b7b6cc2a9f33c21dfccc3b705381
loren pabst <loren pabst>
2017-06-21 10:53:03 -0600

    app.js is ready for routing properly now

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import router from './router.js';
 
 import './reset.css';
 import './App.css';
-import './components/VideoPage/VideoPage.css';
 
 class App extends Component {
 
@@ -14,23 +13,6 @@ class App extends Component {
         <header className = 'header_placeholder'></header>
         
         { router }
-
-        {/*<section className='videopage_main_container'>
-
-          <div className='more_videos_placeholder'></div>
-
-          <div className='iframe_placeholder'>
-            <div className='iframe'></div>
-          </div>
-
-          <div className='video_title_wrapper'>
-            <div className='video_title_container'>
-              <h2>Video Title</h2>
-            </div>
-          </div>
-        
-
-        </section>*/}
 
         <footer className='footer_placeholder'></footer>
 
