5badc5bd36b88437f0fb15039b003f4833b32a3e
Jessica <agro-chick@hotmail.com>
2017-06-26 13:54:22 -0600

    'finished the comment section styling'

--- src/components/VideoPage/CommentsContainer/CommentsContainer.js
+++ src/components/VideoPage/CommentsContainer/CommentsContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import './CommentsContainer.css';
+import bullet from './../../Header/img/bullet.png'
+import userImg from './../../Header/img/photo.jpg'
 
 class CommentsContainer extends Component {
 
@@ -8,7 +10,21 @@ class CommentsContainer extends Component {
         return (
             <div className='comments_wrapper'>
                 <div className='comments_container'>
-                    <h2>Comments</h2>
+                    <ul>
+                        <li>COMMENTS</li>
+                        <li><img src={ bullet }/></li> 
+                        <li>0</li>
+                    </ul>
+                    <div id="user_img">
+                        <img src={ userImg }/>
+                    </div>
+                    <div id="comment_input_tick">
+                        <div id="comment_input_tick_inner">
+                        </div>
+                    </div>
+                    <div id="comment_input">
+                        <input placeholder="Add a public comment..."/>
+                    </div>
                 </div>
             </div>
         );
