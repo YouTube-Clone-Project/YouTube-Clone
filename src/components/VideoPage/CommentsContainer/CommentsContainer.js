a2dfde2e6063bf3829d30b0276ad4b30fe2b5d7e
loren pabst <loren pabst>
2017-06-29 10:51:45 -0600

    beautified some code in comments container

--- src/components/VideoPage/CommentsContainer/CommentsContainer.js
+++ src/components/VideoPage/CommentsContainer/CommentsContainer.js
@@ -102,35 +102,31 @@ class CommentsContainer extends Component {
                 </div>
                 <section className='all_comments'>
                 {
-                        this.state.comments.map( (comment, index) => {
-                            return  <div key={index} className='individual_comment'>
-                                            <div className='user_comment_thumbnail'></div>
-                                            <ul id="user_info_comment">
-                                                <li id="comment_user_name">{ comment.firstname }</li>
-                                                <li id="comment_posted">{ comment.vid_date }</li>
-                                            </ul>
-                                            <p id="comment_comment">{ comment.content }</p>
-                                            
-                                            <div className='comment_reply_box'>
-                                                <ul id="reply_functions">
-                                                    <li>Reply</li>
-                                                    <li><img src={ bullet }/></li>
-                                                    <li>0</li>
-                                                    <li><div id="thumb_up"></div></li>
-                                                    <li><div id="thumb_down"></div></li>
-                                                </ul>
-                                            </div>
-                                        </div>
+                    this.state.comments.map( (comment, index) => {
+                        return  <div key={index} className='individual_comment'>
+                                    <div className='user_comment_thumbnail'></div>
+                                    <ul id="user_info_comment">
+                                        <li id="comment_user_name">{ comment.firstname }</li>
+                                        <li id="comment_posted">{ comment.vid_date }</li>
+                                    </ul>
+                                    <p id="comment_comment">{ comment.content }</p>
                                     
-                        })
-                    }
-                    </section>
-
+                                    <div className='comment_reply_box'>
+                                        <ul id="reply_functions">
+                                            <li>Reply</li>
+                                            <li><img src={ bullet }/></li>
+                                            <li>0</li>
+                                            <li><div id="thumb_up"></div></li>
+                                            <li><div id="thumb_down"></div></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                    })
+                }
+                </section>
             </div>
         );
     }
 }
 
 export default CommentsContainer;
-
-
